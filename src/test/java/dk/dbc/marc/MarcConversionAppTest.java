4022a68dea54b15e7a33a2da4ce4b637d06f323d
Jacob Christian Poulsen <jp@dbc.dk>
2021-11-24 09:06:35 +0100

    Use system-lambda for captureing Stdout in tests

--- src/test/java/dk/dbc/marc/MarcConversionAppTest.java
+++ src/test/java/dk/dbc/marc/MarcConversionAppTest.java
@@ -15,24 +15,42 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsNot.not;
 
 class MarcConversionAppTest {
     @Test
-    void appLineConcatOutput() throws IOException {
-        final PrintStream out = System.out;
-        try (ByteArrayOutputStream capturedStdout = new ByteArrayOutputStream()) {
-            System.setOut(new PrintStream(capturedStdout));
-            int exitCode=MarcConversionApp.runWith(new String[]{"src/test/resources/marc_collection.xml", "--format=LINE_CONCAT"});
+    void appLineConcatOutput() throws Exception {
+        String capturedStdout = tapSystemOut(() -> {
+            MarcConversionApp.runWith(new String[]{"src/test/resources/marc_collection.xml", "--format=LINE_CONCAT"});
+        });
 
-            assertThat(exitCode, is(0));
-            assertThat(capturedStdout.toString(StandardCharsets.UTF_8.name()),
-                    is(readResourceAsString("src/test/resources/marc_collection.lin_concat",
-                            StandardCharsets.UTF_8)));
-        } finally {
-            System.setOut(out);  // restore System.out
-        }
+        assertThat(capturedStdout, is(readResourceAsString("src/test/resources/marc_collection.lin_concat", StandardCharsets.UTF_8)));
+    }
+
+    @Test
+    void returnZeroOnSucces() {
+        int exitCode=MarcConversionApp.runWith(new String[]{"src/test/resources/marc_collection.xml", "--format=LINE_CONCAT"});
+
+        assertThat( exitCode, is (0));
+    }
+
+    @Test
+    void ReturnNonZerrorOnFail() {
+        int exitCode=MarcConversionApp.runWith(new String[]{"src/test/resources/marc_collection.xml", "--format=NON_EXISTING_FORMAT"});
+
+        assertThat( exitCode, is(not(0)));
+    }
+
+    @Test
+    void appLineOutput() throws Exception {
+        String capturedStdout = tapSystemOut(() -> {
+            MarcConversionApp.runWith(new String[]{"src/test/resources/marc_collection.iso", "-o", "danmarc2", "--format=iso"});
+        });
+
+        assertThat(capturedStdout, is(readResourceAsString("src/test/resources/marc_collection.expected_dm2.iso", StandardCharsets.UTF_8)));
     }
 
     private String readResourceAsString(String resource, Charset encoding) throws IOException {
