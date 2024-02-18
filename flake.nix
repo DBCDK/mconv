e452a5de5bd0d56951aac07f3eaf807ec75dc474
Jan Bauer Nielsen <jbn@dbc.dk>
2024-02-18 13:16:41 +0100

    Relax the sandbox for mvn build

--- flake.nix
+++ flake.nix
@@ -1,5 +1,8 @@
 {
   description = "mconv flake";
+ 
+  # Here be dragons
+  nixConfig.sandbox = "relaxed";
 
   inputs = {
     nixpkgs.url = github:nixos/nixpkgs/nixos-23.05; # Use deprecated channel to get graalvm based on JDK 11
@@ -12,43 +15,13 @@
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
-            (f: p: {
+            (final: prev: {
               # force maven to use graalvm JDK
-              maven = p.maven.override { jdk = p.graalvm-ce; };
+              maven = prev.maven.override { jdk = prev.graalvm-ce; };
             })
           ];
         };
         jdk = pkgs.graalvm-ce;
-
-        maven-repository = (pkgs.buildMaven ./project-info.json).repo;
-
-        # double invocation
-        maven-repository-mconv = pkgs.stdenv.mkDerivation {
-          name = "maven-repository";
-          buildInputs = [ pkgs.maven ];
-          src = ./.; # or fetchFromGitHub, cleanSourceWith, etc
-          buildPhase = ''
-          mvn -Dmaven.repo.local=$out -DskipTests=false package
-          '';
-
-          # keep only *.{pom,jar,sha1,nbm} and delete all ephemeral files
-          # with lastModified timestamps inside
-          installPhase = ''
-          find $out -type f \
-          -name \*.lastUpdated -or \
-          -name resolver-status.properties -or \
-          -name _remote.repositories \
-          -delete
-          '';
-
-          # don't do any fixup
-          dontFixup = true;
-          outputHashAlgo = "sha256";
-          outputHashMode = "recursive";
-          # replace this with the correct SHA256
-          #outputHash = pkgs.lib.fakeSha256;
-          outputHash = "sha256-hWb5pDDaNeroOMm35FnULaQdV2yHm8cGGD66nPcz52g=";
-        };
 
         commonInputs = with pkgs; [
         ];
@@ -73,22 +46,22 @@
           pname = "mconv";
           version = "2.0";
 
+          # Here be dragons.
+          # Turn off the sandbox and give the build internet access,
+          # thus effectively giving up build reproducebility.
+          __noChroot = true;
+
           src = ./.;
 
           buildInputs = commonInputs ++ jvmInputs;
 
-          buildPhase = "mvn --offline -Dmaven.repo.local=${maven-repository-mconv} -V clean";
-          #buildPhase = "mvn -Dmaven.repo.local=.m2 -V -P native verify";
+          buildPhase = "mvn -Dmaven.repo.local=.m2 -V -DskipTests=true -P native verify";
 
-          #installPhase = ''
-         #   mkdir -p $out/bin
-          #  mkdir -p $out/share/java
-#
-#            cp target/scala-2.13/*.jar $out/share/java
-#
-#            makeWrapper ${pkgs.jdk17_headless}/bin/java $out/bin/nix-scala-example \
-#              --add-flags "-cp \"$out/share/java/*\" com.example.nixscalaexample.Main"
-#          '';
+          installPhase = ''
+             mkdir -p $out/bin
+
+             cp cli/target/mconv $out/bin
+          '';
         };
       }
     );
